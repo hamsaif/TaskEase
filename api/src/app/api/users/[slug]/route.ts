87615429b57dbe1822faf3b69f3123a773176406
hamsaif <ilhamilhamilham05@gmail.com>
2025-11-17 22:58:44 +0700

    service ambil satu user

---
+++ api/src/app/api/users/[slug]/route.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@/generated/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+// ambil satu user
+export const GET = async (
+    request: NextRequest,
+    { params }: { params: { slug: string } }
+) => {
+    const id = params.slug;
+
+    const user = await prisma.user.findUnique({
+        where: { id },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            createdAt: true
+        }
+    });
+
+    if (!user) {
+        return NextResponse.json({
+            message: "User tidak ditemukan",
+            success: false
+        }, { status: 404 });
+    }
+
+    return NextResponse.json({
+        message: "User ditemukan",
+        success: true,
+        data: user
+    });
+};
