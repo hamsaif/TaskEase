68cf5370b7faa204d0aee0b8c682e4b409088a3c
hamsaif <ilhamilhamilham05@gmail.com>
2025-11-17 22:45:40 +0700

    ganti gaya penulisan di route.ts :v

--- api/src/app/api/users/route.ts
+++ api/src/app/api/users/route.ts
@@ -4,46 +4,22 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 //  ambil semua user
-export async function GET() {
-  try {
+export const GET = async () => {
     const users = await prisma.user.findMany({
-      include: {
-        categories: true,
-        tasks: true,
-      },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            createdAt: true
+        }
     });
 
-    return NextResponse.json(users, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Gagal mengambil user" },
-      { status: 500 }
-    );
-  }
-}
-
-// untuk membuat user baru
-export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
-
-    const user = await prisma.user.create({
-      data: {
-        name: body.name,
-        email: body.email,
-        password: body.password,
-      },
+    return NextResponse.json({
+        message: "Berhasil mengambil data user",
+        success: true,
+        data: users
     });
+};
 
-    return NextResponse.json(user, { status: 201 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Gagal membuat user" },
-      { status: 500 }
-    );
-  }
-}
 
 
