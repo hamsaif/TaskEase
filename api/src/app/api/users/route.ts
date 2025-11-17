b8d466ce11f69d3436b94d32ebb2595503e483e2
hamsaif <ilhamilhamilham05@gmail.com>
2025-11-17 22:56:21 +0700

    menambahkan service create user

--- api/src/app/api/users/route.ts
+++ api/src/app/api/users/route.ts
@@ -21,5 +21,35 @@ export const GET = async () => {
     });
 };
 
+// cerate user
+export const POST = async (request: NextRequest) => {
+    const body = await request.json();
 
+    // Cek email sudah ada
+    const check = await prisma.user.findUnique({
+        where: { email: body.email },
+        select: { id: true }
+    });
+
+    if (check) {
+        return NextResponse.json({
+            message: "Email sudah digunakan",
+            success: false
+        }, { status: 400 });
+    }
+
+    const create = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email,
+            password: body.password
+        }
+    });
+
+    return NextResponse.json({
+        message: "User berhasil dibuat",
+        success: true,
+        data: create
+    });
+};
 
