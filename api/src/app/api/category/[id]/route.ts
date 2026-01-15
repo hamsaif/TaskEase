8db333ef351cddcc0d39bffa90b710c73e36abfc
hamsaif <ilhamilhamilham05@gmail.com>
2026-01-15 13:52:01 +0700

    service ambil 1 category

---
+++ api/src/app/api/category/[id]/route.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@/generated/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+// service ambil 1 category
+export async function GET(
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+    // ambil id
+  const { id } = await context.params;
+  const categoryId = Number(id);
+    // cek apakah id valid
+  if (isNaN(categoryId)) {
+    // jika id tidak valid
+    return NextResponse.json(
+      { success: false, message: "ID tidak valid" },
+      { status: 400 }
+    );
+  }
+//   cek apakah category ada
+  const category = await prisma.category.findUnique({
+    where: { id: categoryId }
+  });
+    // jika category tidak ada
+  if (!category) {
+    return NextResponse.json(
+      { success: false, message: "Kategori tidak ditemukan" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({
+    success: true,
+    data: category
+  });
+}
