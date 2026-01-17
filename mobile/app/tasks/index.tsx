fd1a8aa30d2786f9c128425eaf2c061d388bb85d
hamsaif <ilhamilhamilham05@gmail.com>
2026-01-17 16:51:46 +0700

    index task

--- mobile/app/tasks/index.tsx
+++ mobile/app/tasks/index.tsx
@@ -1,2 +1,27 @@
-// List task screen
 
+import { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import { TaskService } from "@/services/task.service";
+import { Task } from "@/types/task";
+
+export default function TaskList() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    TaskService.getAll().then(res => {
+      if (res.success) setTasks(res.data);
+    });
+  }, []);
+
+  return (
+    <FlatList
+      data={tasks}
+      keyExtractor={item => item.id.toString()}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item.title}</Text>
+        </View>
+      )}
+    />
+  );
+}
